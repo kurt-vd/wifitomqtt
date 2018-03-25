60d615618f94f404fdca5f1a2d109ddf24bac091
Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
2018-03-25 21:56:30 +0200

    import wifitomqtt

---
+++ Makefile
@@ -0,0 +1,25 @@
+PROGS	= wifitomqtt
+default	: $(PROGS)
+
+PREFIX	= /usr/local
+
+CC	= gcc
+CFLAGS	= -Wall
+CPPFLAGS= -D_GNU_SOURCE
+LDLIBS	= -lmosquitto
+INSTOPTS= -s
+
+VERSION := $(shell git describe --tags --always)
+
+-include config.mk
+
+# avoid overruling the VERSION
+CPPFLAGS += -DVERSION=\"$(VERSION)\"
+
+wifitomqtt: libet/libt.o
+
+install: $(PROGS)
+	$(foreach PROG, $(PROGS), install -vp -m 0777 $(INSTOPTS) $(PROG) $(DESTDIR)$(PREFIX)/bin/$(PROG);)
+
+clean:
+	rm -rf $(wildcard *.o libet/*.o) $(PROGS)
---
+++ wifitomqtt.c
@@ -0,0 +1,762 @@
+/*
+ * Copyright 2018 Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <ctype.h>
+#include <errno.h>
+#include <math.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <locale.h>
+#include <poll.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <mosquitto.h>
+
+#include "libet/libt.h"
+
+#define NAME "wifitomqtt"
+#ifndef VERSION
+#define VERSION "<undefined version>"
+#endif
+
+/* generic error logging */
+void mylog(int loglevel, const char *fmt, ...)
+{
+	va_list va;
+
+	va_start(va, fmt);
+	//vsyslog(loglevel, fmt, va);
+	vfprintf(stderr, fmt, va);
+	va_end(va);
+	fputc('\n', stderr);
+	if (loglevel <= LOG_ERR)
+		exit(1);
+	fflush(stderr);
+}
+#define ESTR(num)	strerror(num)
+
+/* program options */
+static const char help_msg[] =
+	NAME ": Control wpa-supplicant via MQTT\n"
+	"usage:	" NAME " [OPTIONS ...] [FILE|DEVICE]\n"
+	"\n"
+	"Options\n"
+	" -V, --version		Show version\n"
+	" -v, --verbose		Be more verbose\n"
+	"\n"
+	" -h, --host=HOST[:PORT]Specify alternate MQTT host+port\n"
+	" -i, --iface=IFACE	Control IFACE (default: wlan0)\n"
+	"\n"
+	"Arguments\n"
+	" FILE|DEVICE	Read input from FILE or DEVICE\n"
+	;
+
+#ifdef _GNU_SOURCE
+static struct option long_opts[] = {
+	{ "help", no_argument, NULL, '?', },
+	{ "version", no_argument, NULL, 'V', },
+	{ "verbose", no_argument, NULL, 'v', },
+
+	{ "host", required_argument, NULL, 'h', },
+	{ "iface", required_argument, NULL, 'i', },
+
+	{ },
+};
+#else
+#define getopt_long(argc, argv, optstring, longopts, longindex) \
+	getopt((argc), (argv), (optstring))
+#endif
+static const char optstring[] = "Vv?h:i:";
+
+/* signal handler */
+static volatile int sigterm;
+static volatile int ready;
+
+/* MQTT parameters */
+static const char *mqtt_host = "localhost";
+static int mqtt_port = 1883;
+static int mqtt_keepalive = 10;
+static int mqtt_qos = -1;
+
+/* state */
+static struct mosquitto *mosq;
+__attribute__((format(printf,2,3)))
+static void publish_value(const char *value, const char *topicfmt, ...);
+__attribute__((format(printf,1,2)))
+static const char *valuetostr(const char *fmt, ...);
+
+/* WPA */
+static const char *iface = "wlan0";
+static int wpasock;
+
+/* signalling */
+static int mysignal(int signr, void (*fn)(int))
+{
+	struct sigaction sa = {
+		.sa_handler = fn,
+	};
+	return sigaction(signr, &sa, NULL);
+}
+static void onsigterm(int signr)
+{
+	sigterm = 1;
+}
+
+static void my_exit(void)
+{
+	if (mosq)
+		mosquitto_disconnect(mosq);
+}
+
+/* WPA */
+struct str {
+	struct str *next;
+	char a[1];
+};
+
+static struct str *strq, *strqlast;
+
+static void add_strq(const char *a)
+{
+	struct str *str;
+
+	str = malloc(sizeof(*str)+strlen(a));
+	if (!str)
+		mylog(LOG_ERR, "malloc str: %s", ESTR(errno));
+	strcpy(str->a, a);
+
+	/* linked list */
+	if (strqlast)
+		strqlast->next = str;
+	else
+		strq = str;
+	strqlast = str;
+	str->next = NULL;
+}
+
+static struct str *pop_strq(void)
+{
+	struct str *head;
+
+	head = strq;
+	if (head)
+		strq = strq->next;
+	if (!strq)
+		strqlast = NULL;
+	return head;
+}
+
+/* networks */
+struct network {
+	int id;
+	char *ssid;
+	char *psk;
+};
+
+static struct network *networks;
+static int nnetworks, snetworks;
+static struct network *newnetwork;
+
+static int networkcmp(const void *a, const void *b)
+{
+	const struct network *na = a, *nb = b;
+
+	return strcmp(na->ssid, nb->ssid);
+}
+
+static struct network *find_network_by_ssid(const char *ssid)
+{
+	struct network needle = {
+		.ssid = (char *)ssid,
+	};
+
+	return bsearch(&needle, networks, nnetworks, sizeof(*networks), networkcmp);
+}
+
+static struct network *add_network(int num, const char *ssid)
+{
+	struct network *net;
+
+	if (nnetworks+1 > snetworks) {
+		snetworks += 16;
+		networks = realloc(networks, sizeof(*networks)*snetworks);
+		if (!networks)
+			mylog(LOG_ERR, "realloc %i networks: %s", snetworks, ESTR(errno));
+	}
+	net = &networks[nnetworks++];
+
+	net->id = num;
+	net->ssid = strdup(ssid);
+	return net;
+}
+
+static inline void sort_networks(void)
+{
+	qsort(networks, nnetworks, sizeof(*networks), networkcmp);
+}
+
+struct ap {
+	char bssid[20];
+	char *ssid;
+	int freq;
+	int level;
+	int flags;
+		#define BF_WPA		0x01
+		#define BF_KNOWN	0x02
+};
+
+static struct ap *aps;
+static int naps, saps;
+
+static int apbssidcmp(const void *a, const void *b)
+{
+	const struct ap *aap = a, *bap = b;
+
+	return strcmp(aap->bssid, bap->bssid);
+}
+
+static struct ap *find_ap_by_bssid(const char *bssid)
+{
+	struct ap needle;
+
+	strncpy(needle.bssid, bssid, sizeof(needle.bssid));
+	return bsearch(&needle, aps, naps, sizeof(*aps), apbssidcmp);
+}
+
+static void add_ap(const char *bssid, int freq, int level, const char *flags, const char *ssid)
+{
+	struct ap *ap;
+
+	if (naps+1 > saps) {
+		saps += 16;
+		aps = realloc(aps, sizeof(*aps)*saps);
+		if (!aps)
+			mylog(LOG_ERR, "realloc %i aps: %s", saps, ESTR(errno));
+	}
+	ap = &aps[naps++];
+	strncpy(ap->bssid, bssid, sizeof(ap->bssid));
+	ap->freq = freq;
+	ap->level = level;
+	/* TODO: flags */
+	ap->ssid = strdup(ssid);
+	if (find_network_by_ssid(ssid))
+		ap->flags |= BF_KNOWN;
+	qsort(aps, naps, sizeof(*aps), apbssidcmp);
+}
+
+static void remove_ap(const char *bssid)
+{
+	struct ap *ap = find_ap_by_bssid(bssid);
+
+	if (!ap)
+		return;
+	/* handle memory */
+	free(ap->ssid);
+
+	/* remove element */
+	int idx = ap - aps;
+	if (idx != naps-1)
+		memcpy(ap, ap+1, (naps-1-idx)*sizeof(*aps));
+	--naps;
+}
+
+/* wpa functions */
+static inline int mystrncmp(const char *needle, const char *haystack)
+{
+	return strncmp(needle, haystack, strlen(needle));
+}
+
+static void wpa_keepalive(void *);
+__attribute__((format(printf,1,2)))
+static int wpa_send(const char *fmt, ...)
+{
+	va_list va;
+	char *line;
+	int ret;
+
+	va_start(va, fmt);
+	vasprintf(&line, fmt, va);
+	va_end(va);
+
+	ret = send(wpasock, line, strlen(line), 0);
+	if (ret < 0)
+		mylog(LOG_ERR, "send wpa: %s", ESTR(errno));
+	add_strq(line);
+	libt_add_timeout(5, wpa_keepalive, NULL);
+	fprintf(stderr, "> %s\n", line);
+	return ret;
+}
+
+static void wpa_keepalive(void *dat)
+{
+	wpa_send("PING");
+}
+
+static void wpa_recvd_pkt(char *line)
+{
+	int ret, j;
+	char *tok, *saveptr;
+	struct str *head;
+
+	ret = strlen(line);
+	line[ret] = 0;
+	if (line[ret-1] == '\n')
+		line[ret-1] = 0;
+	fprintf(stderr, "< %s\n", line);
+
+	if (!mystrncmp("<2>", line) || !mystrncmp("<3>", line)) {
+		/* publish line+3 to mqtt log */
+		publish_value(line+3, "tmp/%s/wpa", iface);
+		tok = strtok(line+3, " \t");
+		if (!strcmp(tok, "CTRL-EVENT-CONNECTED")) {
+			wpa_send("STATUS");
+		} else if (!strcmp(tok, "CTRL-EVENT-DISCONNECTED")) {
+			wpa_send("STATUS");
+		} else if (!strcmp(tok, "CTRL-EVENT-BSS-ADDED")) {
+			strtok(NULL, " \t");
+			tok = strtok(NULL, " \t");
+			wpa_send("BSS %s", tok);
+		} else if (!strcmp(tok, "CTRL-EVENT-BSS-REMOVED")) {
+			strtok(NULL, " \t");
+			tok = strtok(NULL, " \t");
+			remove_ap(tok);
+
+			publish_value("", "net/%s/ap/%s/freq", iface, tok);
+			publish_value("", "net/%s/ap/%s/level", iface, tok);
+			publish_value("", "net/%s/ap/%s/ssid", iface, tok);
+		}
+		return;
+	}
+	head = pop_strq();
+	if (!head) {
+		mylog(LOG_WARNING, "unsolicited response '%s'", line);
+		return;
+	}
+	if (!strcmp(line, "FAIL") || !strcmp(line, "UNKNOWN COMMAND")) {
+		mylog(LOG_WARNING, "'%s': %.30s", head->a,  line);
+	} else if (!strcmp(head->a, "ATTACH")) {
+		mylog(LOG_INFO, "wpa connected");
+
+		wpa_send("LIST_NETWORKS");
+		wpa_send("SCAN_RESULTS");
+		wpa_send("STATUS");
+
+	} else if (!strcmp("LIST_NETWORKS", head->a)) {
+		/* clear network list */
+		for (j = 0; j < nnetworks; ++j)
+			free(networks[j].ssid);
+		nnetworks = 0;
+
+		/* parse lines */
+		int id;
+		char *ssid;
+		for (line = strtok_r(line, "\r\n", &saveptr); line;
+				line = strtok_r(NULL, "\r\n", &saveptr)) {
+			if (!mystrncmp("network id", line))
+				/* header line */
+				continue;
+			id = strtoul(strtok(line, "\t"), NULL, 0);
+			ssid = strtok(NULL, "\t");
+			add_network(id, ssid);
+		}
+		sort_networks();
+
+	} else if (!strcmp(head->a, "SCAN_RESULTS")) {
+		/* clear ap list */
+		for (j = 0; j < naps; ++j)
+			free(aps[j].ssid);
+		naps = 0;
+
+		/* parse lines */
+		for (line = strtok_r(line, "\r\n", &saveptr); line;
+				line = strtok_r(NULL, "\r\n", &saveptr)) {
+			if (!mystrncmp("bssid", line))
+				/* header line */
+				continue;
+			/* process like 'hot-detected' bssid's */
+			wpa_send("BSS %s", strtok(line, "\t"));
+		}
+
+	} else if (!mystrncmp("BSS ", head->a)) {
+		char *bssid = NULL;
+		char *ssid = NULL;
+		char *flags = NULL;
+		int freq = 0, level = 0;
+		char *val;
+		for (line = strtok_r(line, "\r\n", &saveptr); line;
+				line = strtok_r(NULL, "\r\n", &saveptr)) {
+			tok = strtok(line, "=");
+			val = strtok(NULL, "=");
+
+			if (!strcmp(tok, "bssid"))
+				bssid = val;
+			else if (!strcmp(tok, "freq"))
+				freq = strtoul(val, NULL, 0);
+			else if (!strcmp(tok, "level"))
+				level = strtol(val, NULL, 0);
+			else if (!strcmp(tok, "flags"))
+				flags = val;
+			else if (!strcmp(tok, "ssid"))
+				ssid = val;
+		}
+		add_ap(bssid, freq, level, flags, ssid);
+
+		publish_value(valuetostr("%.3lfG", freq*1e-3), "net/%s/ap/%s/freq", iface, bssid);
+		publish_value(valuetostr("%i", level), "net/%s/ap/%s/level", iface, bssid);
+		publish_value(ssid, "net/%s/ap/%s/ssid", iface, bssid);
+	} else if (!strcmp("STATUS", head->a)) {
+		char *val;
+		char *bssid = NULL, *ssid = NULL;
+		int freq = 0;
+
+		for (line = strtok_r(line, "\r\n", &saveptr); line;
+				line = strtok_r(NULL, "\r\n", &saveptr)) {
+			tok = strtok(line, "=");
+			val = strtok(NULL, "=");
+			if (!strcmp(tok, "bssid"))
+				bssid = val;
+			else if (!strcmp(tok, "ssid"))
+				ssid = val;
+			else if (!strcmp(tok, "freq"))
+				freq = strtoul(val, NULL, 0);
+		}
+		publish_value(bssid, "net/%s/bssid", iface);
+		publish_value(valuetostr("%.3lfG",freq*1e-3), "net/%s/freq", iface);
+		struct ap *ap = find_ap_by_bssid(bssid ?: "");
+		if (ap)
+			publish_value(valuetostr("%i", ap->level), "net/%s/level", iface);
+		publish_value(ssid, "net/%s/ssid", iface);
+
+	} else if (!mystrncmp("ADD_NETWORK", head->a)) {
+		int id;
+		struct network *net;
+
+		id = strtoul(line, NULL, 0);
+		net = newnetwork;
+		newnetwork = NULL;
+
+		if (!net)
+			goto done;
+		net->id = id;
+		wpa_send("SET_NETWORK %i ssid \"%s\"", id, net->ssid);
+		if (net && net->psk) {
+			wpa_send("SET_NETWORK %i psk \"%s\"", id, net->psk);
+			free(net->psk);
+			net->psk = NULL;
+		}
+		wpa_send("ENABLE_NETWORK %i", id);
+	} else if (!mystrncmp("SET_NETWORK ", head->a)) {
+		struct str *str;
+		for (str = strq; str; str = str->next) {
+			if (!mystrncmp("SET_NETWORK", str->a) ||
+					!mystrncmp("ENABLE_NETWORK", str->a) ||
+					!mystrncmp("DISABLE_NETWORK", str->a) ||
+					!mystrncmp("ADD_NETWORK", str->a))
+				break;
+		}
+		if (!str)
+			wpa_send("SAVE_CONFIG");
+	} else {
+		mylog(LOG_INFO, "'%.20s' OK", head->a);
+	}
+done:
+	free(head);
+}
+
+static int wpa_connect(const char *iface)
+{
+	int ret, sock;
+	struct sockaddr_un name = {
+		.sun_family = AF_UNIX,
+	};
+
+	/* open client socket */
+	ret = sock = socket(PF_UNIX, SOCK_DGRAM, 0);
+	if (ret < 0)
+		mylog(LOG_ERR, "socket unix: %s", ESTR(errno));
+	/* connect to server */
+	sprintf(name.sun_path, "/var/run/wpa_supplicant/%s", iface);
+	ret = connect(sock, (struct sockaddr *)&name, SUN_LEN(&name));
+	if (ret < 0)
+		mylog(LOG_ERR, "connect %s: %s", name.sun_path, ESTR(errno));
+
+	/* bind to abstract name */
+	memset(name.sun_path, 0, sizeof(name.sun_path));
+	sprintf(name.sun_path+1, "wpa-mqtt-%s-%i", iface, getpid());
+	ret = bind(sock, (struct sockaddr *)&name, sizeof(name));
+	if (ret < 0)
+		mylog(LOG_ERR, "bind @%s: %s", name.sun_path+1, ESTR(errno));
+	return sock;
+}
+
+/* MQTT API */
+static char *myuuid;
+static const char selfsynctopic[] = "tmp/selfsync";
+static void send_self_sync(struct mosquitto *mosq)
+{
+	int ret;
+
+	asprintf(&myuuid, "%i-%li-%i", getpid(), time(NULL), rand());
+
+	ret = mosquitto_subscribe(mosq, NULL, selfsynctopic, mqtt_qos);
+	if (ret)
+		mylog(LOG_ERR, "mosquitto_subscribe %s: %s", selfsynctopic, mosquitto_strerror(ret));
+	ret = mosquitto_publish(mosq, NULL, selfsynctopic, strlen(myuuid), myuuid, mqtt_qos, 0);
+	if (ret < 0)
+		mylog(LOG_ERR, "mosquitto_publish %s: %s", selfsynctopic, mosquitto_strerror(ret));
+}
+static int is_self_sync(const struct mosquitto_message *msg)
+{
+	return !strcmp(msg->topic, selfsynctopic) &&
+		!strncmp(myuuid ?: "", msg->payload ?: "", msg->payloadlen);
+}
+
+static void my_mqtt_msg(struct mosquitto *mosq, void *dat, const struct mosquitto_message *msg)
+{
+	int ret;
+
+	if (is_self_sync(msg))
+		ready = 1;
+
+	char **toks;
+	int ntoks;
+
+	ret = mosquitto_sub_topic_tokenise(msg->topic, &toks, &ntoks);
+	if (ret < 0)
+		mylog(LOG_ERR, "mosquitto tokenize %s: %s", msg->topic, mosquitto_strerror(ret));
+	if (ntoks < 3 || strcmp("net", toks[0]) || strcmp(iface, toks[1])) {
+	} else if (!strcmp(toks[2], "raw")) {
+		wpa_send(msg->payload);
+	} else if (!strcmp(toks[2], "scan")) {
+		wpa_send("SCAN");
+	} else if (!strcmp(toks[2], "ssid") && ntoks >= 4 && !strcmp(toks[3], "set")) {
+		/* select new ssid. Do this only for new msgs (!retained) */
+		if (msg->payloadlen && strcmp(msg->payload, "all")) {
+			struct network *net;
+
+			net = find_network_by_ssid(msg->payload ?: "");
+			if (net)
+				wpa_send("SELECT_NETWORK %i", net->id);
+			else
+				mylog(LOG_INFO, "selected unknown network '%s'", (char *)msg->payload ?: "");
+		} else
+			wpa_send("ENABLE_NETWORK all");
+	} else if (ntoks >= 4 && !strcmp(toks[3], "psk")) {
+		struct network *net;
+
+		net = find_network_by_ssid(toks[2]);
+		if (!net) {
+			if (newnetwork) {
+				/* too rapid */
+				mylog(LOG_WARNING, "previous new ssid still pending");
+				return;
+			}
+			wpa_send("ADD_NETWORK");
+			net = add_network(-1, toks[2]);
+			net->psk = strndup(msg->payload, msg->payloadlen);
+			sort_networks();
+			/* sorting invalidates 'net' pointer */
+			newnetwork = find_network_by_ssid(toks[2]);
+		} else {
+			wpa_send("SET_NETWORK %i psk \"%s\"", net->id, (char *)msg->payload);
+		}
+	}
+	mosquitto_sub_topic_tokens_free(&toks, ntoks);
+}
+
+static void publish_value(const char *value, const char *topicfmt, ...)
+{
+	va_list va;
+	int ret;
+	static char topic[1024];
+
+	va_start(va, topicfmt);
+	vsprintf(topic, topicfmt, va);
+	va_end(va);
+
+	/* publish cache */
+	ret = mosquitto_publish(mosq, NULL, topic, strlen(value ?: ""), value, mqtt_qos, 1);
+	if (ret < 0)
+		mylog(LOG_ERR, "mosquitto_publish %s: %s", topic, mosquitto_strerror(ret));
+}
+
+static const char *valuetostr(const char *fmt, ...)
+{
+	va_list va;
+	static char value[128];
+
+	va_start(va, fmt);
+	vsprintf(value, fmt, va);
+	va_end(va);
+
+	return value;
+}
+
+static void subscribe_topic(const char *topicfmt, ...)
+{
+	va_list va;
+	int ret;
+	char *topic;
+
+	va_start(va, topicfmt);
+	vasprintf(&topic, topicfmt, va);
+	va_end(va);
+
+	/* publish cache */
+	ret = mosquitto_subscribe(mosq, NULL, topic, mqtt_qos);
+	if (ret < 0)
+		mylog(LOG_ERR, "mosquitto_subscribe %s: %s", topic, mosquitto_strerror(ret));
+}
+
+
+int main(int argc, char *argv[])
+{
+	int opt, ret;
+	char *str;
+	char mqtt_name[32];
+	int logmask = LOG_UPTO(LOG_NOTICE);
+	struct pollfd pf[2];
+
+	setlocale(LC_ALL, "");
+	/* argument parsing */
+	while ((opt = getopt_long(argc, argv, optstring, long_opts, NULL)) >= 0)
+	switch (opt) {
+	case 'V':
+		fprintf(stderr, "%s %s\nCompiled on %s %s\n",
+				NAME, VERSION, __DATE__, __TIME__);
+		exit(0);
+	case 'v':
+		switch (logmask) {
+		case LOG_UPTO(LOG_NOTICE):
+			logmask = LOG_UPTO(LOG_INFO);
+			break;
+		case LOG_UPTO(LOG_INFO):
+			logmask = LOG_UPTO(LOG_DEBUG);
+			break;
+		}
+		break;
+	case 'h':
+		mqtt_host = optarg;
+		str = strrchr(optarg, ':');
+		if (str > mqtt_host && *(str-1) != ']') {
+			/* TCP port provided */
+			*str = 0;
+			mqtt_port = strtoul(str+1, NULL, 10);
+		}
+		break;
+	case 'i':
+		iface = optarg;
+		break;
+
+	default:
+		fprintf(stderr, "unknown option '%c'", opt);
+	case '?':
+		fputs(help_msg, stderr);
+		exit(1);
+		break;
+	}
+
+	atexit(my_exit);
+	mysignal(SIGINT, onsigterm);
+	mysignal(SIGTERM, onsigterm);
+	openlog(NAME, LOG_PERROR, LOG_LOCAL2);
+	setlogmask(logmask);
+
+	/* WPA */
+	wpasock = wpa_connect(iface);
+	wpa_send("ATTACH");
+	/* MQTT start */
+	if (mqtt_qos < 0)
+		mqtt_qos = !strcmp(mqtt_host ?: "", "localhost") ? 0 : 1;
+	mosquitto_lib_init();
+	sprintf(mqtt_name, "%s-%i", NAME, getpid());
+	mosq = mosquitto_new(mqtt_name, true, NULL);
+	if (!mosq)
+		mylog(LOG_ERR, "mosquitto_new failed: %s", ESTR(errno));
+	/* mosquitto_will_set(mosq, "TOPIC", 0, NULL, mqtt_qos, 1); */
+
+	ret = mosquitto_connect(mosq, mqtt_host, mqtt_port, mqtt_keepalive);
+	if (ret)
+		mylog(LOG_ERR, "mosquitto_connect %s:%i: %s", mqtt_host, mqtt_port, mosquitto_strerror(ret));
+	mosquitto_message_callback_set(mosq, my_mqtt_msg);
+	subscribe_topic("net/%s/#", iface);
+
+	/* prepare poll */
+	pf[0].fd = wpasock;
+	pf[0].events = POLL_IN;
+	pf[1].fd = mosquitto_socket(mosq);
+	pf[1].events = POLL_IN;
+
+	while (!sigterm) {
+		libt_flush();
+		ret = poll(pf, 2, libt_get_waittime());
+		if (ret < 0 && errno == EINTR)
+			continue;
+		if (ret < 0)
+			mylog(LOG_ERR, "poll ...");
+		if (pf[0].revents) {
+			static char line[1024*16];
+			/* read input events */
+			ret = recv(wpasock, line, sizeof(line)-1, 0);
+			if (ret < 0 && errno == EINTR)
+				continue;
+			if (ret < 0)
+				mylog(LOG_ERR, "recv wpa: %s", ESTR(errno));
+			if (!ret) {
+				mylog(LOG_WARNING, "wpa closed");
+				break;
+			}
+			line[ret] = 0;
+			wpa_recvd_pkt(line);
+		}
+		if (pf[1].revents) {
+			/* mqtt read ... */
+			ret = mosquitto_loop_read(mosq, 1);
+			if (ret)
+				mylog(LOG_ERR, "mosquitto_loop_read: %s", mosquitto_strerror(ret));
+		}
+		/* mosquitto things to do each iteration */
+		ret = mosquitto_loop_misc(mosq);
+		if (ret)
+			mylog(LOG_ERR, "mosquitto_loop_misc: %s", mosquitto_strerror(ret));
+		if (mosquitto_want_write(mosq)) {
+			ret = mosquitto_loop_write(mosq, 1);
+			if (ret)
+				mylog(LOG_ERR, "mosquitto_loop_write: %s", mosquitto_strerror(ret));
+		}
+	}
+
+	/* terminate */
+	send_self_sync(mosq);
+	while (!ready) {
+		ret = mosquitto_loop(mosq, 10, 1);
+		if (ret < 0)
+			mylog(LOG_ERR, "mosquitto_loop: %s", mosquitto_strerror(ret));
+	}
+
+	return 0;
+}
